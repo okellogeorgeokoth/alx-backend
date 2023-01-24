e41512b46cf8a13d6454c3010865370a3a994b21
GEORGE OKELLO <45455816+okellogeorgeokoth@users.noreply.github.com>
2023-01-24 23:45:18 +0300

    Create 100-lfu_cache.py

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,19 @@
+def put(self, key, item):
+    if key is None or item is None:
+        return None
+    if key in self.cache_data:
+        self.frequency[key] += 1
+    else:
+        self.frequency[key] = 1
+    self.cache_data[key] = item
+    if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+        lfu = min(self.frequency, key=self.frequency.get)
+        self.frequency.pop(lfu)
+        self.cache_data.pop(lfu)
+        print("DISCARD: " + lfu)
+        
+def get(self, key):
+    if key is None or key not in self.cache_data:
+        return None
+    self.frequency[key] += 1
+    return self.cache_data[key]
